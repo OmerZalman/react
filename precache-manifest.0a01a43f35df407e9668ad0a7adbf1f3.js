90c007a5a5a99620ab25a1f8f23e87c072ac2bcc
omerzalman <omerzalman@gmail.com>
2019-05-29 00:31:32 +0300

    Updates

---
+++ precache-manifest.0a01a43f35df407e9668ad0a7adbf1f3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "aebbbdef8ae6639f7b20",
+    "url": "/react/static/css/main.57c4ca2a.chunk.css"
+  },
+  {
+    "revision": "aebbbdef8ae6639f7b20",
+    "url": "/react/static/js/main.0c69d480.chunk.js"
+  },
+  {
+    "revision": "4e6a4b677d1c1e6ba359",
+    "url": "/react/static/js/runtime~main.bb50a20d.js"
+  },
+  {
+    "revision": "638e7f8442526910d111",
+    "url": "/react/static/css/2.a747fd4a.chunk.css"
+  },
+  {
+    "revision": "638e7f8442526910d111",
+    "url": "/react/static/js/2.4ba8ee9f.chunk.js"
+  },
+  {
+    "revision": "672e5fb8a344d166ea8270178fd9cda5",
+    "url": "/react/index.html"
+  }
+];
